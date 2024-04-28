10b0d96d1fe408f39980d700d50c07ff6cb11a9d
piru <121550328+erpirulin@users.noreply.github.com>
2024-04-28 15:17:24 +0200

    Create .py

---
+++ .py
@@ -0,0 +1,10 @@
+# Tomar el número de espacios disponibles como entrada
+spaces = int(input())
+
+# Mostrar mensaje si hay espacios disponibles
+if spaces > 0:
+    print("Available spaces")
+
+# Mostrar un mensaje diferente si no hay espacios disponibles
+else:
+    print("Sorry, the parking lot is full")
